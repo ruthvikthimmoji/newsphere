b01510edb251a62a9b211b4d85cc449740bf07bb
Ruthviikthimmoji <149005776+ruthvikthimmoji@users.noreply.github.com>
2025-03-09 10:23:32 +0530

    final commit

--- lib/features/bookmarks/presentation/bookmark_page.dart
+++ lib/features/bookmarks/presentation/bookmark_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:news_sphere/core/services/bookmark_service.dart';
 
+import '../../../core/widgets/article_veiw.dart';
+
 class BookmarksPage extends StatefulWidget {
   const BookmarksPage({super.key});
 
@@ -29,6 +31,59 @@ class _BookmarksPageState extends State<BookmarksPage> {
       articles = newArticles;
       _isLoading = false;
     });
+  }
+
+  void showArticleDialog(
+      BuildContext context, Map<String, dynamic> article) async {
+    bool isBookmarked = await BookmarkService.isBookmarked(article);
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return StatefulBuilder(
+          builder: (context, setDialogState) {
+            return AlertDialog(
+              title: Text(article['title']),
+              content: Text(article['content'] ?? 'No content available'),
+              actions: [
+                IconButton(
+                  onPressed: () async {
+                    if (isBookmarked) {
+                      await BookmarkService.removeBookmark(article);
+                    } else {
+                      await BookmarkService.addBookmark(article);
+                    }
+                    setDialogState(() {
+                      isBookmarked = !isBookmarked;
+                    });
+                  },
+                  icon: Icon(
+                    isBookmarked ? Icons.bookmark : Icons.bookmark_outline,
+                    color: isBookmarked ? Colors.blue : Colors.black,
+                  ),
+                ),
+                SizedBox(width: 80.0),
+                IconButton(
+                  onPressed: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) =>
+                            ArticleView(articleUrl: article['url']),
+                      ),
+                    );
+                  },
+                  icon: Icon(Icons.open_in_new),
+                ),
+                TextButton(
+                  onPressed: () => Navigator.pop(context),
+                  child: const Text('Close'),
+                )
+              ],
+            );
+          },
+        );
+      },
+    );
   }
 
   @override
@@ -81,6 +136,9 @@ class _BookmarksPageState extends State<BookmarksPage> {
                         maxLines: 2, overflow: TextOverflow.ellipsis),
                     subtitle: Text(article['description'] ?? 'No Description',
                         maxLines: 2, overflow: TextOverflow.ellipsis),
+                    onTap: () {
+                      showArticleDialog(context, article);
+                    },
                   ),
                 );
               },
